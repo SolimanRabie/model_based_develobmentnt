a4430e9b752816febd1bdd467131b6692e066c4e
SolimanRabie <151523341+SolimanRabie@users.noreply.github.com>
2024-02-06 14:26:51 +0200

    Add files via upload

---
+++ MATLAP_Basics/cell_array.m
@@ -0,0 +1,7 @@
+cell_arrray={"appel",1,[1 2 3],"coco"};
+cell_arrray{end+1}="moksha";
+disp(cell_arrray);
+arr={rand(3,3),magic(4)};
+disp(arr{1});
+nc={{1,2,3},"appel",{'moz','bassal'}};
+disp(nc(2));
