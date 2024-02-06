0715bdea0f76a77a8886ebb72570652ae4f839aa
SolimanRabie <151523341+SolimanRabie@users.noreply.github.com>
2024-02-07 00:01:56 +0200

    Add files via upload

---
+++ MATLAP_Basics/plot_ex_1.m
@@ -0,0 +1,12 @@
+x=linspace(-2,2,10);
+y=x.^2;
+plot(x,y,'LineWidth',2,'Color','c','LineStyle','--','Marker','*','MarkerSize',8);
+xlabel('x_axies');
+ylabel('y_axies');
+title("plotting");
+ 
+
+
+
+ 
+
